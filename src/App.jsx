e30ec1b58f9a0cb812e265bc80de5421e7cda45e
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-23 20:52:07 -0300

    Se llama al componente Formulario.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
 import './App.css'
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <>
+      <section className='mt-4'>
+        <Container>
+          <Formulario></Formulario>
+        </Container>
+      </section>
     </>
   )
 }
