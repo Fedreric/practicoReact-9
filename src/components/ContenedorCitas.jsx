7ae088d011a479e6f8722a8d2d93eb12dadedeba
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-24 21:03:40 -0300

    Se agregan los componentes contenedorCitas y Cita con su maquetado de ejemplo

---
+++ src/components/ContenedorCitas.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import Cita from "./Cita";
+
+const ContenedorCitas = () => {
+    return (
+        <Container className="row">
+            <Cita></Cita>
+        </Container>
+    );
+};
+
+export default ContenedorCitas;
